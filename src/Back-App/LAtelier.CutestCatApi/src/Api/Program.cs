c1f62832d2275543f582ac5d6e7a32b46bfc2149
Clere-Nicephore NKOUNKOU <=>
2024-04-11 01:37:45 +0200

    fix swagger

--- src/Back-App/LAtelier.CutestCatApi/src/Api/Program.cs
+++ src/Back-App/LAtelier.CutestCatApi/src/Api/Program.cs
@@ -25,11 +25,8 @@ builder.Services.AddScoped<IVotesRepository, VotesRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 //Global server error handle middleware
 app.UseServerErrorHandler();
